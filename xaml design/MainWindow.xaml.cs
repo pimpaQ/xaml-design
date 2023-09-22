25f3bfa209a05fede0b604994e6d6cc9ea8ca80b
pimpaQ <yamaletidel@gmail.com>
2023-09-22 22:37:06 +0500

    ДОбавил корректность вводимых данных

--- xaml design/MainWindow.xaml.cs
+++ xaml design/MainWindow.xaml.cs
@@ -24,5 +24,68 @@ namespace xaml_design
         {
             InitializeComponent();
         }
+
+        private void Registr_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginBox.Text.Trim();
+            string password = PasswordBox.Password.Trim();
+            string password2 = repPassw.Password.Trim();
+            string email = EmailBox.Text.ToLower().Trim();
+            int count = 0;
+
+            if(login.Length < 5)
+            {
+                LoginBox.ToolTip = "Логин должен быть не менее 5-ти символов";
+                LoginBox.Background = Brushes.DarkRed;
+                count++;
+            }
+            else
+            {
+                LoginBox.ToolTip = "";
+                LoginBox.Background = Brushes.Transparent;
+            }
+
+            if(password.Length < 5 || password.Any(Char.IsUpper))
+            {
+                PasswordBox.ToolTip = "Пароль должен быть не менее 5-ти символов";
+                PasswordBox.Background = Brushes.DarkRed;
+                count++;
+            }
+            else
+            {
+                PasswordBox.ToolTip = "";
+                PasswordBox.Background = Brushes.Transparent;
+
+            }
+
+            if (password != password2 || password2.Length < 5)
+            {
+
+                repPassw.ToolTip = "Пароли должны совпадать";
+                repPassw.Background = Brushes.DarkRed;
+                count++;
+            }
+            else
+            {
+                repPassw.ToolTip = "";
+                repPassw.Background = Brushes.Transparent;
+            }
+            if (email.Length < 5 || !email.Contains('@') || !email.Contains('.'))
+            {
+
+                EmailBox.ToolTip = "Email введён неккоректно";
+                EmailBox.Background = Brushes.DarkRed;
+                count++;
+            }
+            else
+            {
+                EmailBox.ToolTip = "";
+                EmailBox.Background = Brushes.Transparent;
+            }
+            if(count == 0)
+            {
+                MessageBox.Show("Вы успешно зарегистрировались");
+            }
+        }
     }
 }
